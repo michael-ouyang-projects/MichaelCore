2596ce6c4589cb10007600ef1d2cd9ca17280a7e
Michael Ouyang <michaelouyang19971997@gmail.com>
2020-12-10 15:23:09 +0800

    Update MyTest.java

--- src/test/java/demo/test/MyTest.java
+++ src/test/java/demo/test/MyTest.java
@@ -1,10 +1,11 @@
 package demo.test;
 
+import org.junit.jupiter.api.Test;
+
 import demo.user.controller.UserController;
 import demo.user.service.UserService;
 import demo.user.service.UserServiceAsync;
 import tw.framework.michaelcore.test.annotation.Spy;
-import tw.framework.michaelcore.test.annotation.Test;
 import tw.framework.michaelcore.test.annotation.TestingClass;
 
 public class MyTest {
@@ -19,7 +20,7 @@ public class MyTest {
     private UserServiceAsync userServiceAsync;
 
     @Test
-    public void test() throws Exception {
+    public void test() {
 
     }
 
